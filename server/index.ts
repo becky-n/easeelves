5f50db38abcc751494e15f334428a334d64b2b28
Becky <beckyngan28@gmail.com>
2025-02-22 03:27:12 +1300

    setting up project

---
+++ server/index.ts
@@ -0,0 +1,34 @@
+import express, { json } from "express";
+import cors from "cors";
+import router from "./routes/routes";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+
+config();
+
+// Create your express server here
+const app = express();
+
+// Configure your express server here
+const PORT = process.env.PORT ?? 4000;
+
+// Start your express server here
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Backend!');
+});
+
+// app.use(json());
+// app.use(express.static("public"));
+
+// // Connect to the database here
+// app.use("/", router);
+
+// Initialise your routes here
+mongoose.connect(process.env.MONGO_URL ?? "").then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+});
